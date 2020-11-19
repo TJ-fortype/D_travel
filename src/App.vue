48463ddc726fb3131bf2353a26361a70a07b9f0a
TJ-fortype <1193103568@qq.com>
2020-11-19 23:00:29 +0800

    初始化

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+	<TopBar></TopBar>
+
+</template>
+
+<script>
+	import TopBar from './components/TopBar.vue'
+	export default {
+		components:{TopBar,}
+	}
+</script>
+
+<style>
+	.container {
+		width: 1200;
+		margin: 0 auto;
+	}
+	
+</style>
---
+++ src/components/TopBar.vue
@@ -0,0 +1,24 @@
+<template>
+<div class="topbar">
+	<div class="container">
+		<div class="logo"></div>
+		<ul>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+		</ul>
+		<div class="heart"></div>
+	</div>
+</div>
+</template>
+
+<script>
+	export default {
+		
+	}
+</script>
+
+<style>
+	
+</style>
