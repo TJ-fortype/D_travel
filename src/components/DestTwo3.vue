a06efe92e636f6812ff18428ebb860dc64834a47
iweb <251786148@qq.con>
2020-11-26 08:09:24 +0800

    赫尔辛基

---
+++ src/components/DestTwo3.vue
@@ -0,0 +1,86 @@
+<template>
+	<div class="destmain3">
+		<div class="destmaintop">
+			<p class="destmaintop1">赫尔辛基地区的更多景点</p>
+			<p clsaa="destmaintop2">通过“我的行程选择”发现活动体验、住宿以及更多资讯</p>
+		</div>
+		<ul class="destmainfoot">
+			<li>
+				<div class="destmainfoot11"></div>
+				<div class="destmainfoot2">
+				<p>赫尔辛基皮艇游</p>	
+				</div>
+				<div class="destmainfoot3"></div>
+			</li>
+			
+			<li>
+				<div class="destmainfoot12"></div>
+				<div class="destmainfoot2">
+					<p>波尔沃木屋老城</p>
+				</div>
+				<div class="destmainfoot3"></div>
+			</li>
+			<li>
+				<div class="destmainfoot13"></div>
+				<div class="destmainfoot2">
+					<p>在赫尔辛基欣赏歌剧和芭蕾</p>
+				</div>
+				<div class="destmainfoot3"></div>
+			</li>
+			<li>
+				<div class="destmainfoot14"></div>
+				<div class="destmainfoot2">
+					<p>‘蒸汽’桑拿房</p>
+				</div>
+				<div class="destmainfoot3"></div>
+			</li>
+		</ul>
+	</div>
+</template>
+
+<script>
+</script>
+
+<style>
+	.destmain3{
+		width: 100%;
+		height: 800px;
+		border: 1px solid #F97C80;
+	}
+	.destmaintop1{
+		padding-top: 30px;
+		font-size: 20px;
+		font-weight: bold;
+	}
+	.destmainfoot{
+		width: 1200px;
+		height: 600px;
+		display: flex;
+		margin: 40px auto;
+		border: 1px solid #FC5113;
+	}
+	.destmainfoot li{
+		flex: 1;
+		margin: 20px 10px;
+	}
+	.destmainfoot11{
+		width: 280px;
+		height: 180px;
+		background: url(../assets/img/dest/small02.jpg)no-repeat  center;
+	}
+	.destmainfoot12{
+		width: 280px;
+		height: 180px;
+		background: url(../assets/img/dest/small01.jpg)no-repeat  center;
+	}
+	.destmainfoot13{
+		width: 280px;
+		height: 180px;
+		background: url(../assets/img/dest/small03.jpg)no-repeat  center;
+	}
+	.destmainfoot14{
+		width: 280px;
+		height: 180px;
+		background: url(../assets/img/dest/small04.jpg)no-repeat  center;
+	}
+</style>
--- src/views/Destination.vue
+++ src/views/Destination.vue
@@ -1,12 +1,16 @@
 <template>
 	<DestTwo></DestTwo>
+	<DestTwo2></DestTwo2>
+	<DestTwo3></DestTwo3>
 </template>
 
 <script>
 	 import DestTwo from '../components/DestTwo.vue'
+	  import DestTwo2 from '../components/DestTwo2.vue'
+	   import DestTwo3 from '../components/DestTwo3.vue'
 	 export default{
 		 components:{
-			 DestTwo,
+			 DestTwo,DestTwo2,DestTwo3,
 		 },
 	 }
 </script>
